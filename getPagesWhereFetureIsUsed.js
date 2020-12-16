833c04e9ad38511e33473bc0ff8b01366e5560f7
Alvaro <alvaroenriquehl@gmail.com>
2020-12-16 17:52:04 -0500

    Agregado logica al script para obtener paginas donde se usa un feature

--- getPagesWhereFetureIsUsed.js
+++ getPagesWhereFetureIsUsed.js
@@ -1,3 +1,22 @@
+const featuresList = [
+  "ads-perured/default",
+  "ads/default",
+  "basic-html/default",
+  "breaking-news/default",
+  "custom-image/default",
+  "lifeweek/default",
+  "custom-title/default",
+  "ordered-stories-list/default",
+  "photogallery/default",
+  "tv-highlight/default",
+  "minute-by-minute/default",
+];
+
+// const filePath = "./data/sandbox/pagesInfo.json"
+const filePath = "./data/sandbox/templatesInfo.json";
+
+///////////////////////////////////////////////////////////////////////////////////
+
 const { promises: fs } = require("fs");
 
 async function getContent(filePath, encoding = "utf-8") {
@@ -8,6 +27,20 @@ async function getContent(filePath, encoding = "utf-8") {
 }
 
 (async () => {
-  const featuresList = await getContent("./data/sandbox/featuresList.json");
-  console.log(featuresList);
+  const pagesInfoFile = await getContent(filePath);
+
+  const pagesInfo = JSON.parse(pagesInfoFile);
+
+  const pagesWhereFeatureIsUsed = featuresList.map((feature) => {
+    const result = { feature, pagesList: [] };
+
+    pagesInfo.forEach(({ name, featuresList }) => {
+      if (featuresList !== "No publicado" && featuresList.includes(feature)) {
+        result.pagesList.push(name);
+      }
+    });
+
+    return result;
+  });
+  console.log(pagesWhereFeatureIsUsed);
 })();
